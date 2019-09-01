906dcc70f494fb5bf361ec61f27b98473696a456
Victor Nivet <victor@saumon.ninja>
2019-09-01 20:38:12 +0200

    add "tomorrow" label for dates and fix off-by-one error in day counting

--- bot.py
+++ bot.py
@@ -37,6 +37,7 @@ class Translation:
     EVENTS_INFO_ADDED_BY='Ajouté par'
     EVENTS_INFO_REMAINING_DAYS='dans {0} jours'
     EVENTS_INFO_REMAINING_DAYS_TODAY='aujourd\'hui'
+    EVENTS_INFO_REMAINING_DAYS_TOMORROW='demain'
     EVENTS_INFO_LOCATION='Lieu'
     EVENTS_INFO_LIST_STATUS='{0} **{1}**'
     EVENTS_NEW_ERROR_DATE_FORMAT='Erreur à la création de l\'événement : la date \'{0}\' ne correspond pas au format {1}'
@@ -211,8 +212,10 @@ class Event:
         remaining_days_string = ''
         remaining_days = self.remaining_days()
         if remaining_days is not None:
-            if remaining_days > 0:
+            if remaining_days > 1:
                 remaining_days_string = f' *{Translation.EVENTS_INFO_REMAINING_DAYS.format(remaining_days)}*'
+            elif remaining_days == 1:
+                remaining_days_string = f' *{Translation.EVENTS_INFO_REMAINING_DAYS_TOMORROW}*'
             else:
                 remaining_days_string = f' *{Translation.EVENTS_INFO_REMAINING_DAYS_TODAY}*'
         if self.message is not None:
@@ -352,7 +355,7 @@ class Event:
                 return 0
             else:
                 delta = self.date - today
-                return delta.days
+                return int(delta.days) + 1
         return None
 
 class Tobman:
